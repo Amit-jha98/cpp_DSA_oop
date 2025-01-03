2571e825eee7d9da05e6c6a577a8e3dc31c73c55
Amit-jha98 <sumitkr95078@gmail.com>
2025-01-04 00:01:33 +0530

    Initial commit 2

--- DSA/2D_array/matrix_transpos.cpp
+++ DSA/2D_array/matrix_transpos.cpp
@@ -19,19 +19,22 @@ void printMatrix(int arr[][3], int rows) {
 }
 
 int main() {
-    int matrix[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
+    int arr[3][3];
+    
+    cout << "Enter elements of the 3x3 matrix: " << endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+        }
+    }
 
-    cout << "Original Matrix:" << endl;
-    printMatrix(matrix, 3);
+    cout << "Original matrix: " << endl;
+    printMatrix(arr, 3);
 
-    transpose(matrix, 3);
-
-    cout << "Transposed Matrix:" << endl;
-    printMatrix(matrix, 3);
+    transpose(arr, 3);
+    
+    cout << "Transposed matrix: " << endl;
+    printMatrix(arr, 3);
 
     return 0;
 }
