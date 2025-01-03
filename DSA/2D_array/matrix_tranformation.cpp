d2d665d9f3ea8d23fd187e5413ea93810ac242de
Amit-jha98 <sumitkr95078@gmail.com>
2025-01-04 00:07:06 +0530

    Initial commit 2

---
+++ DSA/2D_array/matrix_tranformation.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Rotate Matrix 90 degrees clockwise
+void rotateMatrix(int arr[][3], int n) {
+    // Step 1: Transpose
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+    // Step 2: Reverse each row
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            swap(arr[i][j], arr[i][n - 1 - j]);
+        }
+    }
+}
+
+// Function to print the matrix
+void printMatrix(int arr[][3], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int arr[3][3];
+
+    cout << "Enter elements of the 3x3 matrix: " << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Original matrix: " << endl;
+    printMatrix(arr, 3);
+
+    rotateMatrix(arr, 3);
+
+    cout << "Matrix after rotating 90 degrees clockwise: " << endl;
+    printMatrix(arr, 3);
+
+    return 0;
+}
