91c5765f9b200ea0a2973c92e80eabf2ea410f63
Amit-jha98 <sumitkr95078@gmail.com>
2025-01-08 21:45:28 +0530

    Initial commit 2

--- DSA/Functions/function_types.cpp
+++ DSA/Functions/function_types.cpp
@@ -1,26 +1,86 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 //....TYPE OF FUNCTION....
 
-//...Basedon return Type...
+//...Based on return Type...
 
-//1st Void Function
-void printspmething() {
-    cout << "How are you.. (-:";
+// 1st Void Function
+void printSomething() {
+    cout << "How are you.. (-:" << endl;
 }
 
-//value-returning Function
-
+// Value-returning Function
 int sum(int a, int b) {
     return a + b;
 }
 
-// Refrence-returning functions
-
+// Reference-returning functions
 int& getElement(vector<int>& arr, int index) {
     return arr[index];
 }
 
-
 //...Based On parameter...
+
+// No Parameters
+void sayHello() {
+    cout << "Hello!" << endl;
+}
+
+// Value Parameters
+int square(int x) {
+    return x * x;
+}
+
+// Reference Parameters
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+// Const Parameters
+double calculateArea(const double radius) {
+    return 3.14159 * radius * radius;
+}
+
+int main() {
+    // Testing the functions
+
+    // Void Function
+    printSomething();
+
+    // Value-returning Function
+    int result = sum(5, 3);
+    cout << "Sum: " << result << endl;
+
+    // Reference-returning Function
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    // Modify the third element
+    getElement(arr, 2) = 10; 
+    cout << "Modified array: ";
+    for (int i : arr) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // No Parameters
+    sayHello();
+
+    // Value Parameters
+    int sq = square(4);
+    cout << "Square: " << sq << endl;
+
+    // Reference Parameters
+    int x = 10, y = 20;
+    swap(x, y);
+    cout << "Swapped values: x = " << x << ", y = " << y << endl;
+
+    // Const Parameters
+    double area = calculateArea(5.0);
+    cout << "Area: " << area << endl;
+
+    return 0;
+}
