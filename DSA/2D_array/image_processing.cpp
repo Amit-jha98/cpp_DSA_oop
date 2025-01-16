c69f117585b584093ce7173a66e46fc6a1958efc
Amit-jha98 <sumitkr95078@gmail.com>
2025-01-17 00:02:59 +0530

    Initial commit 2

--- DSA/2D_array/image_processing.cpp
+++ DSA/2D_array/image_processing.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 #include <string>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h" // Include the header file
+#include "stb_image_write.h"
 
 using namespace std;
 
-const int width = 256;  // Image width
-const int height = 256; // Image height
-unsigned char image[height][width][3]; // Color image array (RGB)
+const int width = 256;
+const int height = 256;
+unsigned char image[height][width][3];
 
-// Function to initialize the image with colorful patterns
 void initializeColorfulImage() {
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
-            image[j][i][0] = i;           // Red channel (gradient horizontally)
-            image[j][i][1] = j;           // Green channel (gradient vertically)
-            image[j][i][2] = (i + j) % 256; // Blue channel (diagonal gradient)
+            image[j][i][0] = i;
+            image[j][i][1] = j;
+            image[j][i][2] = (i + j) % 256;
         }
     }
 }
 
 int main() {
-    // Initialize colorful image
     initializeColorfulImage();
 
-    // Ask the user for a file path
     string filePath;
     cout << "Enter the file path to save the colorful image (e.g., Amit.png): ";
     cin >> filePath;
 
-    // Save the image as PNG
     if (stbi_write_png(filePath.c_str(), width, height, 3, image, width * 3)) {
         cout << "Image saved successfully at " << filePath << endl;
     } else {
